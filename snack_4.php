bac4b5b51c8dacf5c6c3ea0b9ca4c960e01eea41
AndreiBurbulia <77996991+AndreiBurbulia@users.noreply.github.com>
2021-06-22 16:39:44 +0200

    Snack 4

--- snack_4.php
+++ snack_4.php
@@ -1,5 +1,14 @@
 <?php
+$array = [];
+while (count($array)  < 15) {
+    $number = rand(1, 100);
 
+    if (!in_array($number, $array)) {
+        $array[] = $number;
+    };
+}
+
+var_dump($array);
 
 
 ?>
@@ -11,9 +20,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Snack 1</title>
+    <title>Snack 4</title>
 </head>
 <body>
-    <h1>Snack 1</h1>
+    <h1>Snack 4</h1>
+    <p>Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta</p>
 </body>
 </html>
